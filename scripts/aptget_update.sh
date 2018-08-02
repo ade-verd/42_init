8f13340edf86ab3f2812c3d3cffe03f2d64607fa
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-08-02 18:30:29 +0200

    04

--- scripts/aptget_update.sh
+++
@@ -1,4 +0,0 @@
-echo ---------------------------- >> /var/log/update_script.log 2>&1
-echo $(TZ='Europe/Paris' date) >> /var/log/update_script.log 2>&1
-echo ---------------------------- >> /var/log/update_script.log 2>&1
-sudo apt-get update -y >> /var/log/update_script.log 2>&1
